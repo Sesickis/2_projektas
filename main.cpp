0cddaba80dc67067a7e7fff2ce27fc9201cfaff0
Valdemaras <sesickis@proton.me>
2026-04-10 15:31:18 +0300

    DI variantas

--- main.cpp
+++ main.cpp
@@ -1,102 +1,219 @@
 #include <iostream>
+#include <string>
 #include <iomanip>
-
 using namespace std;
-const float GBP_Bendras = 0.8729,
-    GBP_Pirkti    = 0.8600,
-    GBP_Parduoti  = 0.9220,
-    USD_Bendras   = 1.1793,
-    USD_Pirkti    = 1.1460,
-    USD_Parduoti  = 1.2340,
-    INR_Bendras   = 104.6918,
-    INR_Pirkti    = 101.3862,
-    INR_Parduoti  = 107.8546;
 
-int main () {
-    int operacija, valiuta;
-    float kiekis, rezultatas;
+const int MAX_MOKINIU = 100;
+const int MAX_PAZYMIU = 10;
 
-    while (true) {
-        cout<<"\nGalimos operacijos su Eurais: 1 - palyginimas su kita valiuta, 2 - pirkimas, 3 - pardavimas.\n"
-              "Pasirinkite operacija:"<<endl;
-        cin>>operacija;
-         if (operacija != 1 && operacija != 2 && operacija != 3 ) {
-             cout<<"Nera tokios operacijos"<<endl;
-                 return 0;
-         }
-        cout<<"Galimos valiutos: 1 - GBP, 2 - USD, 3 - INR.\n "
-        "Pasirinkite valiuta"<<endl;
-        cin>>valiuta;
-        if (valiuta != 1 && valiuta != 2 && valiuta != 3 ) {
-            cout<<"Nera tokios valiutos"<<endl;
-            return 0;
-        }
-        cout<<"Iveskite valiutos kieki:"<<endl;
-        cin>>kiekis;
-        if (kiekis >0)
-        switch (operacija) {
-            case 1:
-                switch (valiuta) {
-                    case 1:
-                        rezultatas = kiekis * GBP_Bendras;
-                        cout<<kiekis<<" EUR palyginus yra "<<fixed<< setprecision(2) <<rezultatas<<" GBP. "<<endl;
-                        break;
-                    case 2:
-                        rezultatas = kiekis * USD_Bendras;
-                        cout<<kiekis<<" EUR palyginus yra "<<fixed<< setprecision(2) <<rezultatas<<" USD. "<<endl;
-                        break;
-                    case 3:
-                        rezultatas = kiekis * INR_Bendras;
-                        cout<<kiekis<<" palyginus yra "<<fixed<< setprecision(2) <<rezultatas<<" INR. "<<endl;
-                        break;
-                    default:
-                        cout<<"Ivedete nepalaikoma valiuta"<<endl;
-                }
-                break;
-            case 2:
-                switch (valiuta) {
-                    case 1:
-                        rezultatas = kiekis * GBP_Pirkti;
-                        cout<<"Nusipirkote "<<fixed<< setprecision(2) <<rezultatas<<" GBP "<<endl;
-                        break;
-                    case 2:
-                        rezultatas = kiekis * USD_Pirkti;
-                        cout<<"Nusipirkote "<<fixed<< setprecision(2) <<rezultatas<<" USD "<<endl;
-                        break;
-                    case 3:
-                        rezultatas = kiekis * INR_Pirkti;
-                        cout<<"Nusipirkote "<<fixed<< setprecision(2) <<rezultatas<<" INR "<<endl;;
-                        break;
-                    default:
-                        cout<<"Ivedete nepalaikoma valiuta"<<endl;
-                }
-                break;
-            case 3:
-                switch (valiuta) {
-                    case 1:
-                        rezultatas = kiekis / GBP_Parduoti;
-                        cout<<"Nusipirkote "<<fixed<< setprecision(2) <<rezultatas<<" EUR "<<endl;
-                        break;
-                    case 2:
-                        rezultatas = kiekis / USD_Parduoti;
-                        cout<<"Nusipirkote "<<fixed<< setprecision(2) <<rezultatas<<" EUR "<<endl;
-                        break;
-                    case 3:
-                        rezultatas = kiekis / INR_Parduoti;
-                        cout<<"Nusipirkote "<<fixed<< setprecision(2) <<rezultatas<<" EUR "<<endl;;
-                        break;
-                default:
-                        cout<<"Ivedete nepalaikoma valiuta"<<endl;
-                }
-                break;
-            default:
-                cout<<"Tokios operacijos nera";
-        }
-        else {
-            cout<<"Ivedete netinkama skaiciu."<<endl;
-            return 0;
+string vardai[MAX_MOKINIU];
+int pazymiai[MAX_MOKINIU][MAX_PAZYMIU];
+int pazymiuKiekis[MAX_MOKINIU];
+int mokiniuKiekis = 0;
+
+int rastiMokiniPagalVarda(string vardas) {
+    for (int i = 0; i < mokiniuKiekis; i++) {
+        if (vardai[i] == vardas) {
+            return i;
         }
     }
+    return -1;
+}
+
+void pridetiMokini() {
+    if (mokiniuKiekis >= MAX_MOKINIU) {
+        cout << "Negalima prideti daugiau mokiniu.\n";
+        return;
+    }
+
+    string vardas;
+    int kiek;
+
+    cout << "Iveskite mokinio varda: ";
+    cin >> vardas;
+
+    cout << "Kiek pazymiu turi mokinys? (1-10): ";
+    cin >> kiek;
+
+    while (kiek < 1 || kiek > MAX_PAZYMIU) {
+        cout << "Neteisingas kiekis. Iveskite nuo 1 iki 10: ";
+        cin >> kiek;
+    }
+
+    vardai[mokiniuKiekis] = vardas;
+    pazymiuKiekis[mokiniuKiekis] = kiek;
+
+    for (int j = 0; j < kiek; j++) {
+        cout << "Iveskite " << j + 1 << "-aji pazymi: ";
+        cin >> pazymiai[mokiniuKiekis][j];
+
+        while (pazymiai[mokiniuKiekis][j] < 1 || pazymiai[mokiniuKiekis][j] > 10) {
+            cout << "Pazymys turi buti nuo 1 iki 10. Bandykite dar karta: ";
+            cin >> pazymiai[mokiniuKiekis][j];
+        }
+    }
+
+    mokiniuKiekis++;
+    cout << "Mokinys sekmingai pridetas.\n";
+}
+
+void rodytiVisusMokinius() {
+    if (mokiniuKiekis == 0) {
+        cout << "Mokiniu sarasas tuscias.\n";
+        return;
+    }
+
+    cout << "\n--- Visu mokiniu sarasas ---\n";
+    for (int i = 0; i < mokiniuKiekis; i++) {
+        cout << i + 1 << ". " << vardai[i] << " -> ";
+        for (int j = 0; j < pazymiuKiekis[i]; j++) {
+            cout << pazymiai[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+void rodytiVienaMokini() {
+    if (mokiniuKiekis == 0) {
+        cout << "Mokiniu sarasas tuscias.\n";
+        return;
+    }
+
+    string vardas;
+    cout << "Iveskite mokinio varda: ";
+    cin >> vardas;
+
+    int indeksas = rastiMokiniPagalVarda(vardas);
+
+    if (indeksas == -1) {
+        cout << "Toks mokinys nerastas.\n";
+        return;
+    }
+
+    cout << vardai[indeksas] << " pazymiai: ";
+    for (int j = 0; j < pazymiuKiekis[indeksas]; j++) {
+        cout << pazymiai[indeksas][j] << " ";
+    }
+    cout << endl;
+}
+
+void atnaujintiPazymi() {
+    if (mokiniuKiekis == 0) {
+        cout << "Mokiniu sarasas tuscias.\n";
+        return;
+    }
+
+    string vardas;
+    cout << "Iveskite mokinio varda: ";
+    cin >> vardas;
+
+    int indeksas = rastiMokiniPagalVarda(vardas);
+
+    if (indeksas == -1) {
+        cout << "Toks mokinys nerastas.\n";
+        return;
+    }
+
+    cout << "Esami pazymiai: ";
+    for (int j = 0; j < pazymiuKiekis[indeksas]; j++) {
+        cout << "(" << j + 1 << ":" << pazymiai[indeksas][j] << ") ";
+    }
+    cout << endl;
+
+    int nr, naujasPazymys;
+    cout << "Kuri pazymi norite keisti? Iveskite numeri: ";
+    cin >> nr;
+
+    if (nr < 1 || nr > pazymiuKiekis[indeksas]) {
+        cout << "Neteisingas pazymio numeris.\n";
+        return;
+    }
+
+    cout << "Iveskite nauja pazymi: ";
+    cin >> naujasPazymys;
+
+    while (naujasPazymys < 1 || naujasPazymys > 10) {
+        cout << "Pazymys turi buti nuo 1 iki 10. Bandykite dar karta: ";
+        cin >> naujasPazymys;
+    }
+
+    pazymiai[indeksas][nr - 1] = naujasPazymys;
+    cout << "Pazymys atnaujintas sekmingai.\n";
+}
+
+void pasalintiMokini() {
+    if (mokiniuKiekis == 0) {
+        cout << "Mokiniu sarasas tuscias.\n";
+        return;
+    }
+
+    string vardas;
+    cout << "Iveskite mokinio varda, kuri norite pasalinti: ";
+    cin >> vardas;
+
+    int indeksas = rastiMokiniPagalVarda(vardas);
+
+    if (indeksas == -1) {
+        cout << "Toks mokinys nerastas.\n";
+        return;
+    }
+
+    for (int i = indeksas; i < mokiniuKiekis - 1; i++) {
+        vardai[i] = vardai[i + 1];
+        pazymiuKiekis[i] = pazymiuKiekis[i + 1];
+
+        for (int j = 0; j < MAX_PAZYMIU; j++) {
+            pazymiai[i][j] = pazymiai[i + 1][j];
+        }
+    }
+
+    mokiniuKiekis--;
+    cout << "Mokinys pasalintas sekmingai.\n";
+}
+
+void rodytiMeniu() {
+    cout << "\n===== MOKINIU PAZYMIU SISTEMA =====\n";
+    cout << "1. Prideti mokini\n";
+    cout << "2. Rodyti visu mokiniu pazymius\n";
+    cout << "3. Rodyti konkretaus mokinio pazymius\n";
+    cout << "4. Atnaujinti mokinio pazymi\n";
+    cout << "5. Pasalinti mokini\n";
+    cout << "0. Baigti programa\n";
+    cout << "Pasirinkite veiksma: ";
+}
+
+int main() {
+    int pasirinkimas;
+
+    do {
+        rodytiMeniu();
+        cin >> pasirinkimas;
+
+        switch (pasirinkimas) {
+            case 1:
+                pridetiMokini();
+                break;
+            case 2:
+                rodytiVisusMokinius();
+                break;
+            case 3:
+                rodytiVienaMokini();
+                break;
+            case 4:
+                atnaujintiPazymi();
+                break;
+            case 5:
+                pasalintiMokini();
+                break;
+            case 0:
+                cout << "Programa baigta.\n";
+                break;
+            default:
+                cout << "Neteisingas pasirinkimas. Bandykite dar karta.\n";
+        }
+    } while (pasirinkimas != 0);
+
+    return 0;
 }
 //
 // Užduotis „Mokinių pažymių sistema“
